b13c253508e1a7711fa5b657ffad4159cebe25e9
seancorlin <seancorlin@gmail.com>
2017-05-20 08:51:18 -0700

    URLParser is being ad

--- SiteGrabber.py
+++ SiteGrabber.py
@@ -1,13 +1,14 @@
 import csv
 import json
+import re
+import newspaper
 
 
-class csv_reader():
+class csv_reader:
     def __init__(self, fname):
         '''
         Initialize the CSV Reader.
         '''
-
         site_type = []
         json_temp = {}
         with open(fname) as csvfile:
@@ -23,7 +24,7 @@ class csv_reader():
                 else:
                     extra_notes = 'No extra notes for website!'
                 temp_dic = {'Type': site_type, 'Notes': extra_notes}
-                json_temp['http://www.' + row['website']] = temp_dic
+                json_temp['http://www.' + row['website'].replace(' ','')] = temp_dic
                 site_type = []
 
         print('Created')
@@ -67,3 +68,50 @@ class csv_reader():
                 json.dump(dict, outfile)
         except:
             print('Invalid File Name or Failed to Write to File')
+
+
+class Website:
+    def __init__(self, url_):
+        self.__regex = re.compile(
+                r'^(?:http|ftp)s?://' # http:// or https://
+                r'(?:([A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+                r'localhost|' #localhost...
+                r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+                r'(?::\d+)?' # optional port
+                r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+
+        self.webAddress = url_
+        if self.isValidURL(url_) == True:
+            if self.getSource() != 'Invalid Site':
+                self.siteInfo = {
+                    'Source': self.getSource(),
+                    'Articles': self.getArticles(self.webAddress)
+                }
+            else:
+                print('Invalid Site!')
+        else:
+            print('Invalid URL!')
+
+    def getSource(self):
+        ''' Get Source
+        returns the source given a url
+        :return string: 
+        '''
+        try:
+            url_source = re.search(self.__regex, self.webAddress).group(1)[:-1]
+            return url_source if url_source else 'Invalid Site'
+        except:
+            print('Source could not be found...')
+
+    def getArticles(self, url_):
+        list = []
+        temp = newspaper.build(url_)
+        [list.append(article.url) for article in temp.articles]
+        return list
+
+    def isValidURL(self, url_):
+        '''
+        Check if URL is valid. Will be used to observe extra notes.
+        :return bool: 
+        '''
+        return True if self.__regex.search(url_) else False
--- TestCode.py
+++ TestCode.py
@@ -5,7 +5,10 @@ in this folder. it will not be used for final product.
 '''
 
 import newspaper
-from SiteGrabber import csv_reader
+import enchant
+import csv
+from SiteGrabber import csv_reader, Website
+
 
 def main():
     '''
@@ -19,17 +22,67 @@ def main():
 
     # If you run this it will go for a long time as it grabs the top sites from all the sites on the CSV
     my_data = csv_reader('sources.csv')
-    try:
-        for i in my_data.parsed_data:
-            x = newspaper.build(i)
-            for article in x.articles:
-                print(article.url)
-                #print(i, '\t\t', my_data.parsed_data[i])
-    except:
-        print('Web Error')
+    sources = my_data.getSources()
+    dataset_final = {}
+    '''
+    
+    dataset_final = {
+        'Article: {
+            'Source: string,
+            'Url': string,
+            'Title': string,
+            'Date': string,
+            'Authors': list,
+            'Content': string
+        },
+        
+    }
+    
+    Write to CSV
+    
+                Source | URL | Title | Authors | Posted Date | Authors | Content
+    Article 1
+    Article 2
+    
+     '''
+    with open('dataset.csv', 'w') as csvfile:
+        fieldnames = ['Source', 'Type','URL','Title','Authors','Date','Content']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for s in sources:
+            site_ = Website(s)
+            for link_ in site_.siteInfo['Articles']:
+                try:
+                    print('Parsing Article to CSV...')
+                    article = newspaper.Article(link_)
+                    article.download()
+                    article.parse()
 
-    print(my_data.getNotes('FreeBeacon.com'))
+                    writer.writerow({
+                        'Source': site_.siteInfo['Source'],
+                        'Type':   my_data.getTypes(s),
+                        'URL':    link_,
+                        'Title':  article.title,
+                        'Authors':article.authors,
+                        'Date':   article.publish_date,
+                        'Content':article.text
+                    })
+                except:
+                    print('Err...')
+                    pass
+            print('New Site. Adding Links...')
+    '''    
+    site1 = Website(sources[5])
+    print(site1.siteInfo)
+    d = enchant.Dict("en_US")
 
-
+    if site1.siteInfo['Articles']:
+        tempSite = newspaper.Article(site1.siteInfo['Articles'][0])
+        tempSite.download()
+        tempSite.parse()
+        tempSite.nlp()
+        for word in tempSite.keywords:
+            print(word, d.check(word))
+    '''
 if __name__ == "__main__":
     main()
