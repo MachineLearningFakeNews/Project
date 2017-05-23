8cd8d246ebf1db294052a9881088fc628b8bee8b
psychobolt <psychobolt@gmail.com>
2017-05-23 14:43:22 -0700

    Expand type column and print frequencies for a type

--- preprocess.py
+++ preprocess.py
@@ -1,34 +1,66 @@
 import argparse
+import numpy as np
 import pandas as pd
 import unicodedata
 import csv
+import ast
+from itertools import groupby, zip_longest
+from collections import Counter
 
 def preprocess(content):
   # convert non-ASCII to ASCII equivalents. If none, drop them.
   content = unicodedata.normalize('NFC', content).encode('ascii', 'ignore').decode()
   return content
 
+def expand_type_columns(df):
+  keys = ['Type 1', 'Type 2','Type 3']
+  types = np.array([list(types.values()) for types in [dict(zip_longest(keys, ast.literal_eval(row))) for row in df['Type']]])
+  type_columns = dict(zip(keys, types.T))
+  for key in keys:
+    df[key] = type_columns[key]
+  del df['Type']
+  return df
+
+def print_type_frequency(df):
+  cat_frequency = Counter()
+  for row in df['Type']:
+    cat_frequency.update(ast.literal_eval(row))
+  print(cat_frequency)
+  return df
+
+def balance_one_vs_all(df, type_label):
+  return df
+
 parser = argparse.ArgumentParser(description='Reads a CSV data set and preprocesses the content')
-parser.add_argument('--max_rows', type=int, help='Limit number of data rows to preprocess')
+parser.add_argument('--rows', type=int, help='Sample number of data rows to preprocess')
 parser.add_argument('--debug', action='store_true', help='Enable debug print')
 args = parser.parse_args()
 
 with open('trainset.csv', 'w') as csvfile:
-  fieldnames = ['Source', 'Type','URL','Title','Authors','Date','Content']
+  fieldnames = ['Source','Type 1','Type 2','Type 3','URL','Title','Authors','Date','Content']
   writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
   writer.writeheader()
 
   df = pd.read_csv('dataset.csv')
 
+  if args.rows:
+    df = df.sample(n=args.rows, random_state=42)
+
   if args.debug:
-    print('\nTotal: ', df.count, '\n')
+    print('\nTotal: ', df.shape[0])
+    print_type_frequency(df)
+
+  df = expand_type_columns(df)    
+
+  df = balance_one_vs_all(df, 'credible')
+
+  print('')
 
   for index, row in df.iterrows():
-    if (index >= args.max_rows):
-      break
-
     source = row['Source']
-    label_categories = row['Type']
+    type1 = row['Type 1']
+    type2 = row['Type 2']
+    type3 = row['Type 3']
     url = row['URL']
     title = row['Title']
     authors = row['Authors']
@@ -39,7 +71,9 @@ with open('trainset.csv', 'w') as csvfile:
 
     if args.debug:
       print('Source: ', source)
-      print('Type: ', label_categories)
+      print('Type 1: ', type1)
+      print('Type 2: ', type2)
+      print('Type 3: ', type3)
       print('URL: ', url)
       print('Title: ', title)
       print('Authors: ', authors)
@@ -48,7 +82,9 @@ with open('trainset.csv', 'w') as csvfile:
 
     writer.writerow({
       'Source': source,
-      'Type':   label_categories,
+      'Type 1': type1,
+      'Type 2': type2,
+      'Type 3': type3,
       'URL':    url,
       'Title':  title,
       'Authors':authors,
