728d4b55756bd663498bea28b370fe343ac6deb7
psychobolt <psychobolt@gmail.com>
2017-05-27 11:41:20 -0700

    Factorize labels to csv and load it to the training file

--- extract_features.py
+++ extract_features.py
@@ -1,17 +1,19 @@
 import csv
 import pandas as pd
 import numpy as np
-
 from sklearn.feature_extraction.text import TfidfVectorizer
-
 
 df = pd.read_csv('dataset_preprocessed.csv', index_col=False)
 
 articles = df['Content']
+labels = df['Label']
+
 print ('Articles count:', articles.shape[0])
 
 vectorizer = TfidfVectorizer(ngram_range=(2,2), lowercase=False, max_features=3000)
 tf_idf_matrix = vectorizer.fit_transform(articles)
 print ('Matrix shape (articles x features):',  tf_idf_matrix.shape)
 
+label_matrix = pd.DataFrame(pd.factorize(labels)[0])
 np.savez('trainset',data=tf_idf_matrix.data, indices=tf_idf_matrix.indices, indptr=tf_idf_matrix.indptr, shape=tf_idf_matrix.shape)
+label_matrix.to_csv('trainset_labels.csv', index=False)
--- train_model.py
+++ train_model.py
@@ -1,6 +1,11 @@
 import numpy as np
+import pandas as pd
 from scipy.sparse import csr_matrix
+
 
 loader = np.load('trainset.npz')
 train_x = csr_matrix((  loader['data'], loader['indices'], loader['indptr']), shape = loader['shape'])
-print(train_x.shape)
+train_y = pd.read_csv('trainset_labels.csv')
+
+print(train_x.shape, type(train_x)) # train_x is a csr_matrix
+print(train_y.shape, type(train_y)) # train_y is a Pandas Dataframe
