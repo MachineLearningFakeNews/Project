0cf1b5b6dd9bb82debbdc8ea6a48d5fa4a35e8f4
H-yang <tsaihsuany@cpp.edu>
2017-05-29 19:30:44 -0700

    update analyze model and make report table (table will show in terminal)

--- train_model.py
+++ train_model.py
@@ -6,10 +6,13 @@ from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn import metrics
-from sklearn.naive_bayes import BernoulliNB, MultinomialNB
+from sklearn.naive_bayes import MultinomialNB
 from time import time
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression, SGDClassifier
+from texttable import Texttable
+
 
 PATH = os.path.dirname(os.path.abspath(__file__))
 
@@ -28,11 +31,18 @@ def train_svm(train_x, train_y, c_value):
   clf.fit(train_x, train_y.values.ravel())
   return clf
 
-# def train_NB(train_x, train_y, a_values): # which NB?
-#   clf = MultinomialNB(alpha = a_values)
-#   clf.fit(train_x, train_y.values.ravel())
-#   return clf
+def train_default(train_x, train_y, model_name):  
+  if model_name == 'LR':
+    clf = LogisticRegression()
+  elif model_name == 'NB':
+    clf = MultinomialNB()
+  elif model_name == 'SGD':
+    clf = SGDClassifier()
+  else:
+    clf = SVC() # as default
 
+  clf.fit(train_x, train_y.values.ravel())
+  return clf
 
 def evaluate(clf, test_x, test_y):  
   predicted = clf.predict(test_x)
@@ -49,7 +59,7 @@ def analyze_svm(train_x, test_x, train_y, test_y):
   print ('Training SVM model with different C:')
 
   # C = [0.0001, 0.001, 0.01, 0.1, 1, 10]
-  C = [0.0001, 0.001] # quick test
+  C = [2.8] # quick test
   result = []
 
   for c_value in C:
@@ -67,13 +77,54 @@ def analyze_svm(train_x, test_x, train_y, test_y):
   # model name, acc, f1, precision, recall
   # add returned value to a list and use this list to print (plain text or graph)
 
+# def analyze_nb(train_x, test_x, train_y, test_y):
+#   result = []
+#   print ('Training Naive_Bayes model (MultinomialNB)')
+#   clf = train_NB(train_x, train_y)
+#   ret = evaluate(clf, test_x, test_y)
+#   model_used = 'MultinomialNB'
+#   ret += model_used,
+#   result.append(ret)
+#   return result
+
+
+
+def analyze_model(train_x, test_x, train_y, test_y, model_name):
+  result = []
+
+  if model_name != 'SVM':
+    print ('Training model : %s' % model_name)
+    clf = train_default(train_x, train_y, model_name)
+    ret = evaluate(clf, test_x, test_y)
+    model_used = model_name
+    ret += model_used,
+    result.append(ret)
+    return result
+
+  else:
+      print ('Training SVM model with different C:')
+      # C = [0.0001, 0.001, 0.01, 0.1, 1, 10]
+      C = [2.8] # quick test
+      result = []
+
+      for c_value in C:
+        print ('C value: %f' % c_value)
+        clf = train_svm(train_x, train_y, c_value)
+        ret = evaluate(clf, test_x, test_y)
+        print ('----------------------------------------')
+
+        model_used = 'SVM, C: ' + str('%.6f' % c_value)
+        ret += model_used,
+        result.append(ret)
+
+        return result
+
+
 def make_diagram(results):
   indices = np.arange(len(results))
   results = [[x[i] for x in results] for i in range(5)]
   acc, f1, precision, recall, clf_names = results
 
-  # plt.figure(figsize = (12, 8))
-  
   plt.title('Report')
 
   # create plot
@@ -83,13 +134,13 @@ def make_diagram(results):
   plt.barh(indices + .8,  recall, .2,     label = 'recall', color = 'red')
 
   plt.yticks(())
-  plt.legend(loc='best')
-  plt.subplots_adjust(left=.25)
-  plt.subplots_adjust(top=.95)
-  plt.subplots_adjust(bottom=.05)
+  plt.legend(loc = 'best')
+  plt.subplots_adjust(left = .25)
+  plt.subplots_adjust(top = .95)
+  plt.subplots_adjust(bottom = .15)
 
   for i, c in zip(indices, clf_names):
-      plt.text(-.2, i, c)
+      plt.text(-.1, i+.5, c)
 
   plt.show()
 
@@ -102,13 +153,28 @@ if __name__ == '__main__':
 
   train_x, test_x, train_y, test_y = shuffle_split(train_x, train_y)
 
-  svm_result = analyze_svm(train_x, test_x, train_y, test_y)
-  for r in svm_result:
-    print ('Model: ' + r[4])
-    print ('acc: %.4f' % r[0])
-    print ('f1: %.4f' % r[1])
-    print ('precision: %.4f' % r[2])
-    print ('recall: %.4f' % r[3])
-    print ('----------------------------------------')
-    
-  make_diagram(svm_result)
+  # svm_result = analyze_model(train_x, test_x, train_y, test_y, 'SVM')
+  nb_result = analyze_model(train_x, test_x, train_y, test_y, 'NB')
+  lr_result = analyze_model(train_x, test_x, train_y, test_y, 'LR')
+  sgd_result = analyze_model(train_x, test_x, train_y, test_y, 'SGD')
+
+  all_result = []
+  all_result += nb_result
+  all_result += lr_result
+  all_result += sgd_result
+
+  # print tabulate ([ r[4], r[0], r[1], r[2], r[3] ], headers=['Model', 'Acc', 'f1', 'Precision', 'Recall'])
+  
+  # make a table
+  t = Texttable()
+  for r in all_result:
+    # print ('Model: ' + r[4])
+    # print ('acc: %.4f' % r[0])
+    # print ('f1: %.4f' % r[1])
+    # print ('precision: %.4f' % r[2])
+    # print ('recall: %.4f' % r[3])
+    # print ('----------------------------------------')
+    t.add_rows([['Model', 'Acc', 'f1', 'Precision', 'Recall'], [r[4], r[0], r[1], r[2], r[3]]])
+
+  print (t.draw())
+  make_diagram(all_result)
